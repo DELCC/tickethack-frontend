e17982590665db4b041d84c197d505748b1761e5
Fernier <fernier.paul@gmail.com>
2025-09-24 14:26:36 +0200

    front

--- cart.js
+++ cart.js
@@ -1,12 +1,14 @@
 function getTotal(){
-    fetch('http://localhost:3000/reservations/totalCart')
+    return fetch('http://localhost:3000/reservations/totalCart')
                 .then(response=>response.json()
                 .then((data)=>{
                     console.log(data)
                     document.querySelector("#totalCardCart").innerHTML=`
-                    <div id="totalCart">Total: <span>${data.total}€</span></div>
-                    <button class="purchaseButtonCart">Purchase</button>   
-                    </div>`
+                    <div id="totalCart">
+                        Total: <span>${data.total}€</span>
+                    </div>
+                    <a href="./purchase.html"><button id="purchaseButtonCart">Purchase</button></a>   
+                    `
                 })
             )
 }
@@ -33,16 +35,30 @@ fetch('http://localhost:3000/reservations/isBooked')
                     </div>
                     <div class ="newJourneyTimeCart">${schedule.date}</div>
                     <div class ="newJourneyPriceCart">${schedule.price}€</div>
-                    <button class="deleteButton" id="${schedule["_id"]}">X</button>
+                    <a href="./cart.html"><button class="deleteButton" id="${schedule["_id"]}">X</button></a>
                 </div>
                 `
             }
             document.querySelector("#cardCart").innerHTML+=
-                `<div id = "totalCardCart"></div>`
-            getTotal();
+                `<div id = "totalCardCart"></div>`;
+            getTotal()
+            .then(()=>{
+                document.querySelector("#purchaseButtonCart").addEventListener('click', function(){
+                console.log("ca va delete")
+                    fetch('http://localhost:3000/reservations',{
+                        method:'PUT',
+                        headers: { 'Content-Type': 'application/json' },
+                    })
+                    .then(response=>response.json())
+                    .then((data)=>{
+                        console.log(data)
+                    })
+                })
+
+            })
         }
-        
     })
+    
     .then(()=>{
         let deleteArray = document.querySelectorAll(".deleteButton")
         for (const button of deleteArray){
@@ -52,34 +68,34 @@ fetch('http://localhost:3000/reservations/isBooked')
                         headers: { 'Content-Type': 'application/json' },
                 })
                 .then(response => response.json())
-                .then((data)=>{
-                    console.log("ok")
+                .then(()=>{
+                    console.log("deleted")
 
-                    if (data.reservations.length===0){
-                        document.querySelector("#cardCart").innerHTML=`
-                        <div> No Ticket in your cart.</div>
-                        <div>Why not plan a trip</div>`;
-                    } else {
-                        document.querySelector("#cardCart").innerHTML="";
-                        document.querySelector("#cardCart").innerHTML+='<div id="newScheduleListCart"></div>'
-                        for (const schedule of data.reservations){
-                            document.querySelector("#newScheduleListCart").innerHTML+=`
-                            <div class = "newscheduleCart">
-                                <div class ="itineraryCart">
-                                    <div class ="newJourneyDepartureCart">${schedule.departure}</div>
-                                    <div>></div>
-                                    <div id="newJourneyArrivalCart">${schedule.arrival}</div>
-                                </div>
-                                <div class ="newJourneyTimeCart">${schedule.date}</div>
-                                <div class ="newJourneyPriceCart">${schedule.price}€</div>
-                                <button class="deleteButton" id="${schedule["_id"]}">X</button>
-                            </div>
-                            `
-                        }
-                        document.querySelector("#cardCart").innerHTML+=
-                            `<div id = "totalCardCart"></div>`
-                        getTotal();
-                    }
+                    // if (data.reservations.length===0){
+                    //     document.querySelector("#cardCart").innerHTML=`
+                    //     <div> No Ticket in your cart.</div>
+                    //     <div>Why not plan a trip</div>`;
+                    // } else {
+                    //     document.querySelector("#cardCart").innerHTML="";
+                    //     document.querySelector("#cardCart").innerHTML+='<div id="newScheduleListCart"></div>'
+                    //     for (const schedule of data.reservations){
+                    //         document.querySelector("#newScheduleListCart").innerHTML+=`
+                    //         <div class = "newscheduleCart">
+                    //             <div class ="itineraryCart">
+                    //                 <div class ="newJourneyDepartureCart">${schedule.departure}</div>
+                    //                 <div>></div>
+                    //                 <div id="newJourneyArrivalCart">${schedule.arrival}</div>
+                    //             </div>
+                    //             <div class ="newJourneyTimeCart">${schedule.date}</div>
+                    //             <div class ="newJourneyPriceCart">${schedule.price}€</div>
+                    //             <button class="deleteButton" id="${schedule["_id"]}">X</button>
+                    //         </div>
+                    //         `
+                    //     }
+                    //     document.querySelector("#cardCart").innerHTML+=
+                    //         `<div id = "totalCardCart"></div>`
+                    //     getTotal();
+                    // }
                 })
             })
         }
