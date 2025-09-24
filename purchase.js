da968cdd292307f05346c58e4e799d08b3bb628e
Fernier <fernier.paul@gmail.com>
2025-09-24 14:46:26 +0200

    jhjh

--- purchase.js
+++ purchase.js
@@ -1,1 +1,31 @@
+fetch('http://localhost:3000/reservations/isPurchased')
+.then(response=>response.json())
+.then((data)=>{
+    // console.log(data)
+    if (data.purchased.length===0){
+            document.querySelector("#cardPurchase").innerHTML=`<div> No booking yet.</div>
+            <div>Why not plan a trip</div>`;
+        } else {
+            document.querySelector("#cardPurchase").innerHTML+=`
+            <div id="newScheduleListPurchase">
+                <div id = "titlePurchase"> My bookings
+                </div>
+            </div>`
 
+            for (const schedule of data.purchased){
+            console.log(schedule["_id"])
+            document.querySelector("#newScheduleListPurchase").innerHTML+=`
+                <div id = "newschedulePurchase">
+                    <div class ="itineraryPurchase">
+                        <div id="newJourneyDeparturePurchase">${schedule["departure"]}</div>
+                        <div>></div>
+                        <div id="newJourneyArrivalPurchase">${schedule["arrival"]}</div>
+                    </div>
+                    <div id="newJourneyTimePurchase">${schedule["date"]}</div>
+                    <div id="newJourneyPricePurchase">${schedule["price"]}€</div>
+                    <div id="timetogo">${schedule["fromNow"]}</div>
+                </div>`
+            
+            }
+        }
+})
